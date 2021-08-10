939b37b66d324e515d638c57031f6a60d843bde0
foxfurry <isacartur@gmail.com>
2021-08-10 13:26:36 +0300

    Added comments

--- internal/common/database/db_pool.go
+++ internal/common/database/db_pool.go
@@ -8,12 +8,32 @@ import (
 	"time"
 )
 
-func createDatabase(db *sql.DB) {
-	queryDelete := `DROP DATABASE IF EXISTS medialibrary;`
-	queryCreate := `CREATE DATABASE medialibrary
-					ENCODING    'utf8'
-					LC_COLLATE  'en_US.utf8'
-					LC_CTYPE    'en_US.utf8';`
+
+// ifDBExists returns true if dbname exists in database instance.
+// This function works specifically and only with postgres.
+func ifDBExists(db *sql.DB, dbname string) bool {
+	queryCheckExists := `SELECT EXISTS(SELECT datname FROM pg_catalog.pg_database WHERE datname=$1);`
+
+	row := db.QueryRow(queryCheckExists, dbname)
+
+	var status bool
+
+	err := row.Scan(&status)
+
+	if err != nil {
+		log.Panicf("Could not check if db:%v exists: %v", dbname, err)
+	}
+
+	return status
+}
+
+// createDatabase drops dbname from database instance and recreate it with template1 parameters (en_US.utf-8)
+func createDatabase(db *sql.DB, dbname string) {
+	queryDelete := 	`DROP DATABASE IF EXISTS ` + dbname +`;`	// Since postgres does not support parameters for
+	queryCreate := 	`CREATE DATABASE ` + dbname	+				// schema modifiers we have no choice but to concatenate
+					`ENCODING    'utf8'` +						// db name into the query
+					`LC_COLLATE  'en_US.utf8'`+
+					`LC_CTYPE    'en_US.utf8';`
 
 	_, err := db.Query(queryDelete)
 	if err != nil {
@@ -26,7 +46,8 @@ func createDatabase(db *sql.DB) {
 	}
 }
 
-func createTables(db *sql.DB) {
+// Creates a bookstore table(id, title, author, year, description) for a given database instance
+func createBookstore(db *sql.DB) {
 	query := `CREATE TABLE IF NOT EXISTS bookstore (
 					id SERIAL PRIMARY KEY,
 					title TEXT NOT NULL,
@@ -41,30 +62,35 @@ func createTables(db *sql.DB) {
 	}
 }
 
+// CreateDBPool returns database connection pool with specified parameters. Function will validate database and tables
+// before returning the instance
 func CreateDBPool(host string, port int, user string, pass string, dbname string, dbMaxIdleConns int, dbMaxOpenConns int, dbMaxIdleTime time.Duration) *sql.DB {
 	log.Printf("DB configs:\nHost: %v\nPort: %v\nUser: %v\ndbName: %v\nMax idle conns: %v\nMax open conns: %v\nMax idle time: %v",
 		host, port, user, dbname, dbMaxIdleConns, dbMaxOpenConns, dbMaxIdleTime)
-	initTemplate := fmt.Sprintf("host=%s port=%d user=%s password=%s ", host, port, user, pass)
 
-	initTable := fmt.Sprintf("%s dbname=%s sslmode=disable", initTemplate, dbname)
-	initDB := fmt.Sprintf("%s sslmode=disable", initTemplate)
+	initTemplate := fmt.Sprintf("host=%s port=%d user=%s password=%s ", host, port, user, pass) // Connection template
 
-	var err error
+	initTable := fmt.Sprintf("%s dbname=%s sslmode=disable", initTemplate, dbname)	// Connection to specific db
+	initDB := fmt.Sprintf("%s sslmode=disable", initTemplate)						// Generic connection for db create
 
-	db, err := sql.Open("postgres", initDB)
+	db, err := sql.Open("postgres", initDB)	// Connection for db creation
 	if err != nil {
-		log.Panicf("%v", err)
-	}
-	log.Printf("Connected to DB host")
-
-	createDatabase(db)
-
-	db, err = sql.Open("postgres", initTable)
-	if err != nil {
-		log.Panicf("%v", err)
+		log.Panicf("Could not connect to DB: %v", err)
 	}
 
-	createTables(db)
+	if !ifDBExists(db, dbname){	// Since postgres does not allow IF NOT EXISTS for db alter - check and create are standalone
+		log.Printf("Database %v does not exists, creating it", dbname)
+		createDatabase(db, dbname)
+	}else {
+		log.Printf("Database %v already exists, skipping creation", dbname)
+	}
+
+	db, err = sql.Open("postgres", initTable)	// Connection for table creation
+	if err != nil {
+		log.Panicf("Could not connect to db:%v: %v", dbname,err)
+	}
+
+	createBookstore(db)
 
 	db.SetMaxIdleConns(dbMaxIdleConns)
 	db.SetMaxOpenConns(dbMaxOpenConns)
